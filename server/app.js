b6e913b51aa8f66b9f8ee678377e3fe39cc8d99a
Jose <12202517@aluno.cotemig.com.br>
2024-10-07 16:41:08 -0300

    corrigido erro de export do app

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./src/routes/userRoutes');
 const homeRoutes = require('./src/routes/homeRoutes');
+const tokenRoutes = require('./src/routes/tokenRoutes');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/', homeRoutes)
 app.use('/api/users', userRoutes);
-module.exports = router;
-////
+app.use('/api/token/', tokenRoutes);
+module.exports = app;
