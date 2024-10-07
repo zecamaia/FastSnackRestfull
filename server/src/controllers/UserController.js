efc47d2220e1fdd70376d9aa9f7335a45f63205e
Jose <12202517@aluno.cotemig.com.br>
2024-10-07 16:42:51 -0300

    retatoracao e melhoria
    - corrigido retorno de respostas
    - adicionado model da forma correta

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,20 +1,21 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs');
 
 class UserController {
     async store(req, res) {
         try {
-            const { username, email, password, role } = req.body;
+            const {username, email, password, role} = req.body;
             const hashedPassword = await bcrypt.hash(password, 10);
             const novoUsuario = await User.create({
                 username,
                 email,
                 password: hashedPassword
             });
-            return res.json({ id, username, email, role });
+            return res.json({ username, email, role });
 
         } catch (error) {
             console.error(error)
-            return res.status(500).json({ erro: "Erro ao criar usuario" });
+            return res.status(500).json({erro: "Erro ao criar usuario"});
         }
     }
 
@@ -26,7 +27,7 @@ class UserController {
             return res.json(usuarios);
         } catch (error) {
             console.error(error);
-            return res.status(500).json({ erro: "Erro ao listar usuarios" })
+            return res.status(500).json({erro: "Erro ao listar usuarios"})
         }
     }
 
@@ -34,13 +35,13 @@ class UserController {
         try {
             const usuario = await User.findByPk(req.params.id);
             if (!usuario) {
-                return res.status(404).json({ erro: "Usuario nao encontrdo." });
+                return res.status(404).json({erro: "Usuario nao encontrado."});
             }
-            const { id, username, email, role } = usuario;
-            return res.json({ id, username, email, role });
+            const {id, username, email, role} = usuario;
+            return res.json({id, username, email, role});
         } catch (error) {
             console.error(error);
-            return res.status(500).json({ erro: "Erro ao listar usuario" })
+            return res.status(500).json({erro: "Erro ao listar usuario"})
         }
     }
 
@@ -48,14 +49,14 @@ class UserController {
         try {
             const usuario = await User.findByPk(req.params.id);
             if (!usuario) {
-                return res.status(404).json({ erro: "Usuario nao encontrdo." });
+                return res.status(404).json({erro: "Usuario nao encontrado."});
             }
-            const novosDados = await User.update(req.body)
-            const { id, username, email, role } = novosDados;
-            return res.json({ id, username, email, role });
+            const novosDados = await usuario.update(req.body)
+            const {username, email, role} = novosDados;
+            return res.json({ username, email, role});
         } catch (error) {
             console.error(error);
-            return res.status(500).json({ erro: "Erro ao editar usuario" })
+            return res.status(500).json({erro: "Erro ao editar usuario"})
         }
     }
 
@@ -63,7 +64,7 @@ class UserController {
         try {
             const usuario = await User.findByPk(req.params.id);
             if (!usuario) {
-                return res.status(404).json({ erro: "Usuario nao encontrdo." });
+                return res.status(404).json({erro: "Usuario nao encontrado."});
             }
             await usuario.destroy();
             return res.json({
@@ -72,7 +73,7 @@ class UserController {
             });
         } catch (error) {
             console.error(error);
-            return res.status(500).json({ erro: "Erro ao deletar usuario" });
+            return res.status(500).json({erro: "Erro ao deletar usuario"});
         }
     }
 }
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const UserController = require('../controllers/UserController');
+const loginRequired = require("../middlewares/loginRequired");
 const router = express.Router();
 
 router.post("/", UserController.store);
-router.get("/", UserController.index);
+router.get("/",loginRequired, UserController.index);
 router.get("/:id", UserController.show);
 router.put("/:id", UserController.update)
-router.delete("/:id", UserController.update)
+router.delete("/:id", UserController.delete)
 
+module.exports = router;
