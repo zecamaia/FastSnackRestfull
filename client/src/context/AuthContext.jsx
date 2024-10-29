3313c578a8c48aa1bd1810c9a7c32095484ad097
teste <samuelfilipevianadois@gmail.com>
2024-10-29 17:16:04 -0300

    feat: new context login - added new features of persist login and config routes

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const checkAuth = () => {
+        const token = localStorage.getItem('token');
+        const userData = localStorage.getItem('user');
+        if (token && userData) {
+            setIsLoggedIn(true);
+            setUser(JSON.parse(userData));
+        } else {
+            setIsLoggedIn(false);
+            setUser(null);
+        }
+        setLoading(false);
+    }
+
+
+    useEffect(() => {
+        checkAuth();
+    }, []);
+    const login = (token, userData) => {
+        localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(userData));
+        setIsLoggedIn(true);
+        setUser(userData);
+    }
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        setIsLoggedIn(false);
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{ isLoggedIn, user, login, logout, loading }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+export const useAuth = () => useContext(AuthContext);
---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Registro from "../pages/Registro/Registro";
+import Login from "../pages/Login/Login";
+import Layout from "../layout/Layout";
+import ProtectedRoute from "./ProtectedRoute";
+import Evento from "../pages/Evento/Evento";
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            {/* Rotas públicas */}
+            <Route path="/" element={<Home />} />
+            <Route path="/cadastrar" element={<Registro />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Rotas protegidas */}
+            <Route element={<Layout />}>
+                <Route path="/eventos" element={<ProtectedRoute element={<Evento />} />} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default AppRoutes;
