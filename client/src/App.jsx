67da7fe17d14c2bd054a7b125298ee9fb3d60413
teste <samuelfilipevianadois@gmail.com>
2024-10-29 17:15:16 -0300

    feat: added new hooks and context - added new hook for login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import Registro from "./pages/Registro/Registro.jsx";
-import Login from "./pages/Login/Login.jsx";
-import Evento from "./pages/Evento/Evento.jsx";
-import Layout from "./layout/Layout.jsx";
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
-
-  return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/cadastrar' element={<Registro />} />
-      <Route path='/login' element={<Login />} />
-
-      <Route element={<Layout />}>
-        <Route path='/eventos' element={<Evento />} />
-      </Route>
-    </Routes>
-
-
-  )
+  return <AppRoutes />
 }
 
 export default App
