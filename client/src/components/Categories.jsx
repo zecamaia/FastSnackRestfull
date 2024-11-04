e35c2a6420f38491dfe20c6eed422f1737c8ec03
teste <samuelfilipevianadois@gmail.com>
2024-11-04 18:29:52 -0300

    refactor: otimizado e componentizado pagina de categories

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,45 +1,18 @@
 import React from 'react';
-
-const Categories = ({ image, name }) => {
+import { Link } from 'react-router-dom'
+const Categories = ({ categories }) => {
     return (
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 my-6 mt-10'>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
+            {categories.map((category) => (
+                <div key={category.id} className='flex flex-col items-center'>
+                    <Link to={`/eventos/categoria/${category.id}`} className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
+                        <img className="w-16 h-16 rounded-full" src={category.image} alt="imagem-categoria" />
+                    </Link>
+                    <span className='text-center text-sm font-semibold'>{category.name}</span>
                 </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
-                </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
-                </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
-                </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
-                </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-            <div className='flex flex-col items-center'>
-                <div className="w-16 h-16 rounded-full  flex items-center justify-center overflow-hidden mb-2">
-                    <img className="w-16 h-16 rounded-full" src={image} alt="imagem-categoria" />
-                </div>
-                <span className='text-center text-sm font-semibold'>{name}</span>
-            </div>
-        </div>
+            ))
+            }
+        </div >
     );
 }
 
