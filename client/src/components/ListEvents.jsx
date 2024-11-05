d36392d70f0a78a40d08373e187108909f25e5b5
teste <samuelfilipevianadois@gmail.com>
2024-11-05 17:56:06 -0300

    feat: added and improved cards events

--- client/src/components/ListEvents.jsx
+++ client/src/components/ListEvents.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { format } from 'date-fns';
 import { SlLocationPin, SlCalender } from "react-icons/sl";
 import { FaRegCalendarAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,26 +14,30 @@ const ListEvents = ({ events }) => {
         <div className='grid gap-6 mt-10 max-w-6xl mx-auto px-4 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 cursor-pointer'>
             {events.map((event) => (
                 <div key={event.id} className='bg-white p-3 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 max-w-lg'>
-                    <img
-                        src={event.image}
-                        alt="imagem_evento"
-                        className='w-full h-40 object-cover rounded-lg mb-4'
-                    />
-                    <div className="text-left">
-                        <h3 className="text-2xl font-semibold text-gray-800 truncate">{event.name}</h3>
-                        <div className="mt-4 text-md text-gray-500 space-y-1 ">
-                            <p className="flex items-center">
-                                <FaRegCalendarAlt className='text-primary' />
-                                <span className='ml-1'>
-                                    <span className="font-medium">Dia:</span> {new Intl.DateTimeFormat('pt-BR', { month: 'short', day: '2-digit' }).format(new Date(event.start_date))}
-                                </span>
-                            </p>
-                            <p className="flex items-center">
-                                <SlLocationPin className='text-primary' />
-                                <span className="ml-1 font-medium">{event.location}</span>
-                            </p>
+                    <Link to={`/eventos/${event.id}`} >
+
+
+                        <img
+                            src={event.image}
+                            alt="imagem_evento"
+                            className='w-full h-40 object-cover rounded-lg mb-4'
+                        />
+                        <div className="text-left">
+                            <h3 className="text-2xl font-semibold text-gray-800 truncate">{event.name}</h3>
+                            <div className="mt-4 text-md text-gray-500 space-y-1 ">
+                                <p className="flex items-center">
+                                    <FaRegCalendarAlt className='text-primary' />
+                                    <span className='ml-1'>
+                                        <span className="font-medium">Dia:</span> {new Intl.DateTimeFormat('pt-BR', { month: 'short', day: '2-digit' }).format(new Date(event.start_date))}
+                                    </span>
+                                </p>
+                                <p className="flex items-center">
+                                    <SlLocationPin className='text-primary' />
+                                    <span className="ml-1 font-medium">{event.location}</span>
+                                </p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             ))
             }
