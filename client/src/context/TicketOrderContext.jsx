87d21dea1fc12f81ae8c9b1d415a5a7728c43400
teste <samuelfilipevianadois@gmail.com>
2024-11-07 17:05:33 -0300

    refactor: changes in context global tickets

--- client/src/context/TicketOrderContext.jsx
+++ client/src/context/TicketOrderContext.jsx
@@ -2,17 +2,41 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 const TicketOrderContext = createContext();
 export const TicketOrderProvider = ({ children }) => {
-    const savedOrders = JSON.parse(localStorage.getItem("ticketOrders")) || [];
-    const [ticketOrders, setOrderTickets] = useState([]);
+    const [ticketOrders, setTicketOrders] = useState(() => {
+        const savedOrders = JSON.parse(localStorage.getItem("ticketOrders"));
+        return savedOrders || [];
+    });
+    useEffect(() => {
+        if (ticketOrders.length > 0) {
+            localStorage.setItem("ticketOrders", JSON.stringify(ticketOrders));
+        } else {
+            localStorage.removeItem("ticketOrders");
+        }
+    }, [ticketOrders]);
+    const addTicketOrder = (newTicket) => {
+        setTicketOrders((prevOrders) => {
+            const existingTicket = prevOrders.find(
+                (order) => order.ticket_id === newTicket.ticket_id
+            );
 
-    const addTicketOrder = (ticket) => {
-        setOrderTickets((prevOrders) => [...prevOrders, ticket]);
+            if (existingTicket) {
+                // Se o ingresso já existir, atualiza a quantidade
+                return prevOrders.map((order) =>
+                    order.ticket_id === newTicket.ticket_id
+                        ? { ...order, quantity: order.quantity + newTicket.quantity }
+                        : order
+                );
+            } else {
+                // Se for um novo ingresso, adiciona ao estado
+                return [...prevOrders, newTicket];
+            }
+        });
     };
     const removeTicketOrder = (ticketId) => {
-        setOrderTickets((prevOrders) =>
+        setTicketOrders((prevOrders) =>
             prevOrders.filter((order) => order.ticket_id !== ticketId)
         );
-    }
+    };
     return (
         <TicketOrderContext.Provider value={{ ticketOrders, addTicketOrder, removeTicketOrder }}>
             {children}
